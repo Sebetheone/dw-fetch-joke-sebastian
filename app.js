1e763061bb40da533b2aa124e097ac0d7d90e3a0
Sebetheone <sebetheone@gmail.com>
2026-09-08 12:52:01 +0200

    Fetched Jokes, Rick and morty og Star Wars

--- app.js
+++ app.js
@@ -14,3 +14,49 @@ fetch('https://icanhazdadjoke.com', {
         jokeElement.textContent = "Kunne ikke hente en joke lige nu.";
         console.error("Fejl:", error);
     });
+
+fetch('https://rickandmortyapi.com/api/character', {
+    headers: {
+        'Accept': 'application/json'
+    }
+})
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const characters = data.results;
+        const characterList = document.getElementById('character-list 1');
+        characters.forEach(character => {
+            const listItem = document.createElement('li');
+            listItem.textContent = character.name;
+            characterList.appendChild(listItem);
+        });
+    })
+    .catch(error => {
+        const characterList = document.getElementById('character-list 1');
+        characterList.textContent = "Kunne ikke hente karakterer lige nu.";
+        console.error("Fejl:", error);
+    });
+
+fetch('https://swapi.dev/api/people', {
+    headers: {
+        'Accept': 'application/json'
+    }
+})
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const characters = data.results;
+        const characterList = document.getElementById('character-list 2');
+        characters.forEach(character => {
+            const listItem = document.createElement('li');
+            listItem.textContent = character.name;
+            characterList.appendChild(listItem);
+        });
+    })
+    .catch(error => {
+        const characterList = document.getElementById('character-list 2');
+        characterList.textContent = "Kunne ikke hente karakterer lige nu.";
+        console.error("Fejl:", error);
+    });
